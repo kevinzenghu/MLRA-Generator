fe0c3d43fdb1acc99546328b7e6e600c2cdd6bc7
Savannah Niles <savannahniles@gmail.com>
2014-08-21 13:28:12 -0400

    email

--- MLRA-Generator.js
+++ MLRA-Generator.js
@@ -8,6 +8,12 @@ if (Meteor.isClient) {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
+
+      Meteor.call('sendEmail',
+            ['kzh@mit.edu', 'savannah@mit.edu'],
+            'savannah@mit.edu',
+            'MLRA email from ',
+            'Test!');
     }
   });
 }
@@ -15,5 +21,22 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+  });
+
+  Meteor.methods({
+    sendEmail: function (to, from, subject, text) {
+      check([to, from, subject, text], [String]);
+
+      // Let other method calls from the same client start running,
+      // without waiting for the email sending to complete.
+      this.unblock();
+
+      Email.send({
+        to: to,
+        from: from,
+        subject: subject,
+        text: text
+      });
+    }
   });
 }
